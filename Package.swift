ec8131ffd092db55bdc1c5e7e50550edfa09592c
Philippe Bauloye Jiménez <>
2024-08-05 13:01:49 +0200

    Add Swift Package Manager support

--- Package.swift
+++ Package.swift
@@ -1,8 +1,31 @@
-//
-//  Package.swift
-//  QKMRZScanner
-//
-//  Created by Philippe Bauloye Jiménez on 5/8/24.
-//
+// swift-tools-version:5.3
+import PackageDescription
 
-import Foundation
+let package = Package(
+    name: "QKMRZScanner",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "QKMRZScanner",
+            targets: ["QKMRZScanner"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract.git", from: "1.2.5"),
+        .package(url: "https://github.com/Mattijah/QKMRZParser.git", from: "1.1.1")
+    ],
+    targets: [
+        .target(
+            name: "QKMRZScanner",
+            dependencies: ["SwiftyTesseract", "QKMRZParser"],
+            path: "Sources/QKMRZScanner",
+            resources: [
+                .process("Resources/ocrb.traineddata")
+            ]),
+        .testTarget(
+            name: "QKMRZScannerTests",
+            dependencies: ["QKMRZScanner"],
+            path: "Tests/QKMRZScannerTests"),
+    ]
+)
